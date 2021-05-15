6ce4f848523fa42566cb9e9c32c561cf557d5a5f
Boris Sergeev <surgeboris@gmail.com>
2021-05-15 16:14:13 +0300

    Implemented ability to search for a user

--- src/features/auth/user.ts
+++ src/features/auth/user.ts
@@ -1,6 +1,8 @@
 import { compare as bcryptCompare, hash as bcryptHash } from "bcrypt";
 import { DataTypes, Model } from "denodb";
 
+import { queryRaw } from "src/db.ts";
+
 const roles = ["librarian" as const, "reader" as const];
 
 export type Role = (typeof roles)[0];
@@ -39,6 +41,33 @@ export class User extends Model {
   static hash(plaintextPassword: string) {
     return bcryptHash(plaintextPassword);
   }
+  static async search(params: UserSearchParams) {
+    const values: Partial<UserSearchParams> = {};
+    (["login", "lastname"] as const).forEach((k) => {
+      if (params[k]) values[k] = `%${params[k]}%`;
+    });
+    if (params.from) values.from = params.from;
+    if (params.role) values.role = params.role;
+    values.limit = params.limit ?? 20;
+    let query = `FROM ${User.table}`;
+    const whereClauses: string[] = [];
+    if (params.role) whereClauses.push(`role = :role`);
+    if (params.login) whereClauses.push(`login LIKE :login`);
+    if (params.lastname) whereClauses.push(`lastname LIKE :lastname`);
+    const where = whereClauses.join(" AND ");
+    if (where) query += ` WHERE ${where}`;
+    const totalQuery = `SELECT COUNT(*) as c ${query}`;
+    const { from: _f, limit: _l, ...totalValues } = values;
+    const total: number = (await queryRaw(totalQuery, { ...totalValues }))[0].c;
+    query += ` ORDER BY ${User.table}.created_at DESC`;
+    query += ` LIMIT :limit`;
+    if (params.from) query += ` OFFSET :from`;
+    const users = await queryRaw(
+      `SELECT id, login, firstname, middlename, lastname, address ${query}`,
+      values,
+    );
+    return [total, users] as const;
+  }
   hasPassword(plaintextPassword: string) {
     return bcryptCompare(plaintextPassword, this.password);
   }
@@ -54,4 +83,12 @@ interface UserDataRequired {
   lastname: string;
   firstname: string;
   middlename: string;
+}
+
+interface UserSearchParams {
+  role: string;
+  login: string;
+  lastname: string;
+  from: number;
+  limit: number;
 }
